3da471cbe1c77504c263ca174fab757743c854f9
Ruby <gaojr0110@sina.com>
2018-03-30 15:36:35 +0800

    5

--- src/PrintListReversingly/printListReversingly.java
+++ src/PrintListReversingly/printListReversingly.java
@@ -1,39 +1,39 @@
 package PrintListReversingly;
 //输入一个链表的头结点，从尾到头反过来打印出每个结点的值
 
+import java.util.ArrayList;
 import java.util.Stack;
 
-class List{
-    int value;
-    List next;
-    public List(int value){
-        this.value = value;
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val = val;
     }
 }
 
 class Solution1{
-    public static void reverselyprint(List head){
-        if(head.next == null)
-            System.out.println(head.value);
-        if(head == null)
-            return;
-        List temp = head;
-        if(temp.next != null)
-            reverselyprint(temp.next);
-        System.out.print(temp.value + "\t");
+    ArrayList<Integer> result = new ArrayList<>();
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        //ListNode temp = listNode;
+        if(listNode != null) {
+            printListFromTailToHead(listNode.next);
+            result.add(listNode.val);
+        }
+        return result;
     }
 }
 
 class Solution2{
-    public static void reverselyprint(List head){
+    public static void reverselyprint(ListNode head){
         if(head.next == null)
-            System.out.println(head.value);
+            System.out.println(head.val);
         if(head == null)
             return;
         Stack<Integer> stack = new Stack<>();
-        List temp = head;
+        ListNode temp = head;
         while(temp != null) {
-            stack.push(temp.value);
+            stack.push(temp.val);
             temp = temp.next;
         }
         while(!stack.empty()) {
@@ -45,15 +45,15 @@ class Solution2{
 
 public class printListReversingly {
     public static void main(String[] args){
-        List head = new List(0);
-        List temp = head;
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
         for(int i = 1; i < 6; i ++){
-            List list = new List(i);
+            ListNode list = new ListNode(i);
             temp.next = list;
             temp = temp.next;
         }
-        Solution1.reverselyprint(head);
-        System.out.println();
+        Solution1 s = new Solution1();
+        System.out.println(s.printListFromTailToHead(head));
         Solution2.reverselyprint(head);
     }
 }
