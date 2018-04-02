3cbdfefec2657d7829bd12da82b7dc94d9984b16
Ruby <gaojr0110@sina.com>
2018-04-02 10:59:33 +0800

    12

--- src/Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.java
+++ src/Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.java
@@ -5,11 +5,39 @@ package Print1ToMaxOfNDigits;
 
 class Solution{
     public void Print1ToMaxOfNDigits(int n){
-        if(n < 0)
+        if(n <= 0)
             return;
-        char[] number
+        int[] number = new int[n];
+        for(int i = 0; i < 10; i ++){
+            number[0] = i;
+            Print1ToMaxOfNDigitsRecursively(number, n, 0);
+        }
+    }
+
+    private void Print1ToMaxOfNDigitsRecursively(int[] number, int length, int index){
+        if(index == length - 1) {
+            print(number);
+            return;
+        }
+        for(int i = 0; i < 10; i ++){
+            number[index + 1] = i;
+            Print1ToMaxOfNDigitsRecursively(number, length, index + 1);
+        }
+    }
+
+    private void print(int[] number){
+        boolean isBeginning0 = true;
+        for(int i = 0; i < number.length; i ++){
+            if(isBeginning0 && number[i] != 0)
+                isBeginning0 = false;
+            if(!isBeginning0)
+                System.out.print(number[i]);
+        }
     }
 }
 
 public class Print1ToMaxOfNDigits {
+    public static void main(String[] args){
+        new Solution().Print1ToMaxOfNDigits(3);
+    }
 }
