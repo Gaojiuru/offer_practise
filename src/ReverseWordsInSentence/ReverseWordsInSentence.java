19a59bc78f1b48d41492e6209da7b921598aac86
Ruby <gaojr0110@sina.com>
2018-04-19 20:57:38 +0800

    翻转单词旋转

---
+++ src/ReverseWordsInSentence/ReverseWordsInSentence.java
@@ -0,0 +1,38 @@
+package ReverseWordsInSentence;
+//牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。
+// 例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a student.”。Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+
+class Solution{
+    public String ReverseSetence(String str){
+        if(str == null || str.length() == 0)
+            return "";
+        char[] s = str.toCharArray();
+        Reverse(s, 0, s.length - 1);
+        int blank = -1;
+        for(int i = 0; i < s.length; i ++){
+            if(s[i] == ' '){
+                int nextBlank = i;
+                Reverse(s, blank + 1, nextBlank - 1);
+                blank = nextBlank;
+            }
+        }
+        Reverse(s, blank + 1, s.length - 1);
+        return new String(s);
+    }
+
+    private void Reverse(char[] s, int low, int high){
+        while(low < high) {
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low ++;
+            high --;
+        }
+    }
+}
+
+public class ReverseWordsInSentence {
+    public static void main(String[] args){
+        System.out.println(new Solution().ReverseSetence("I am a student."));
+    }
+}
