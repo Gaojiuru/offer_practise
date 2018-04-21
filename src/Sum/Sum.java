85214b995593e138aa8dc4e38c4a156ff8b94d9b
Ruby <gaojr0110@sina.com>
2018-04-21 16:44:56 +0800

    求1+2+3+...+n

---
+++ src/Sum/Sum.java
@@ -0,0 +1,16 @@
+package Sum;
+//求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）
+
+class Solution{
+    public int Sum(int n){
+        int sum = n;
+        boolean ans = (n > 0) && ((sum += Sum(n - 1)) > 0);
+        return sum;
+    }
+}
+
+public class Sum {
+    public static void main(String[] args){
+        System.out.println(new Solution().Sum(3));
+    }
+}
